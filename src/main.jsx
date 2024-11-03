fdb2881429cf38860b889f04ae061045caae2a0d
Vinícius <santsmarcos81@gmail.com>
2024-11-03 10:02:57 -0300

    Configuracao inicial de rota

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { Routes } from "./routes";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Routes />
+    </ThemeProvider>
+  </React.StrictMode>
 )
